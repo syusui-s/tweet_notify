4daad8c675fd0b99146229cd2b1f8a0b2ac8de9a
Syusui SONOICHI <syusui.s@gmail.com>
2012-11-25 13:32:38 +0900

    modify bug in tweet_notify.rb

--- spec
+++ spec
@@ -1,10 +1,10 @@
 ---
 slug: :tweet_notify
 depends:
-  mikutter: 0.2.0.863
+  mikutter: 0.2
   plugin:
   - settings
-version: '1.1'
+version: 1.2.1
 author: syusui_s
 name: ツイート通知
 description: 特定ユーザのツイートをデスクトップにポップアップと通知音で通知します。
--- tweet_notify.rb
+++ tweet_notify.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # tweet_notify : mikutter用ツイート通知プラグイン
-# version 1.2(2012-11-25 12:40 +0900)
+# version 1.2.1(2012-11-25 12:40 +0900)
 # 
 # mikutter version 0.2.1.1066 core/plugin/notify.rbの一部を翻案しています。
 # 詳細は、README.mdをご覧ください。
@@ -17,13 +17,15 @@ Plugin::create(:tweet_notify) do
   
   onupdate do |service, messages|
     if UserConfig[:tweet_notify_bool] then
-      notify_users = UserConfig[:tweet_notify_users].select{|usr| !usr.empty?}
+      notify_users = UserConfig[:tweet_notify_users].select{|usr| not(usr.empty?)}
       # メッセージのユーザーが一致し、リプライやメンションでもなく、起動後にツイートされたものであれば、通知します
       messages = Plugin.filtering(:show_filter, messages.select{|m| notify_users.index(m.idname) and m.receiver.nil? and m[:created] > DEFINED_TIME}).first
       
-      if !messages.empty? then
-        Plugin.call(:play_sound, UserConfig[:tweet_notify_sound]) if !UserConfig[:tweet_notify_sound].empty?
-        Plugin.call(:popup_notify, User::findbyidname(m.message.user.idname),"update \"#{m.message}\"")
+      if not(messages.empty?) then
+        messages.each{|m|
+          Plugin.call(:play_sound, UserConfig[:tweet_notify_sound]) if not(UserConfig[:tweet_notify_sound].empty?)
+          Plugin.call(:popup_notify, User::findbyidname(m.idname),"update \"#{m.message}\"")
+        }
       end
     end
   end
